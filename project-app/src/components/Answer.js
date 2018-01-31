5cefd02b6ba0429fa72cdfada6e0d51ef2d47732
amysews <amyc271@gmail.com>
2018-01-31 17:03:47 +0000

    Added message telling user when no audio file is available

--- project-app/src/components/Answer.js
+++ project-app/src/components/Answer.js
@@ -5,7 +5,7 @@ const Answer = ({ answer }) => {
     return (
         <div className="answer">
             <blockquote>"{answer.text}"</blockquote>
-            { answer.mp3_in_bucket ? <audio src={`https://s3.us-east-2.amazonaws.com/mp3audiostorage-northcoders/a${answer.id}.mp3`} controls /> : null }
+            { answer.mp3_in_bucket ? <audio src={`https://s3.us-east-2.amazonaws.com/mp3audiostorage-northcoders/a${answer.id}.mp3`} controls /> : <p>No audio file available</p> }
             <p className="subtitle">Submitted by {answer.user.first_name} {answer.user.surname} at {moment(answer.time_stamp).format('h:mm:ss a dddd Do MMMM YYYY')}</p>
         </div>
     )
